86dfad57d86c08e304415d4c1cf743447099f337
Kapil Kumar <masterkapilkumar@gmail.com>
2019-03-31 15:11:20 +0530

    Generated excel file for gradesheet

--- credits_calculator.py
+++ credits_calculator.py
@@ -4,6 +4,8 @@ import traceback
 import argparse
 import requests
 import json
+import xlwt
+from xlwt import Workbook
 import sys
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -12,11 +14,14 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class DegreeCalc:
     
-    def __init__(self, kerberos_username, kerberos_password):
+    def __init__(self, kerberos_username, kerberos_password, gradesheet_filename="gradesheet.xlsx"):
         self.url = "https://academics1.iitd.ac.in/Academics/index.php"
         self.username = kerberos_username
         self.password = kerberos_password
+        self.sheet_name = gradesheet_filename
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+        self.departments = ["BB1", "BB5", "CS5"]
+        self.categories = {"BS":"Basic Sciences", "EA":"Engineering Arts and Science", "PL":"Programme-linked", "HU":"Humanities and Social Sciences", "OC":"Open Category", "DC":"Departmental Core", "DE":"Departmental Electives", "PC":"Programme Core", "PE":"Programme Elective", "NR":"Non Graded"}
 
     def get_response(self, url, headers=None, verify=False, type='GET', data="json"):
         if(not headers):
@@ -29,7 +34,26 @@ class DegreeCalc:
                 data = response.text
             return data, response.status_code
     
-    def get_table_from_text(self, data):
+    def generate_gradesheet(self, data):
+        
+        writer = pd.ExcelWriter(self.sheet_name)
+        row=1
+        col=1
+        df = pd.DataFrame(columns=data.columns)
+        df.to_excel(writer,'gradesheet',startcol=col,startrow=0, columns=['Course Code', 'Course Description', 'Course Category', 'Course Credits', 'Grade'], index=False)
+        for category in self.categories:
+            category_name = self.categories[category]
+            df = pd.DataFrame(columns=[category_name])
+            df.to_excel(writer,'gradesheet',startcol=0,startrow=row, columns=[category_name], index=False)
+            row += 1
+            df = data.loc[data['Course Category'] == category]
+            df.to_excel(writer,'gradesheet',startcol=col,startrow=row, columns=['Course Code', 'Course Description', 'Course Category', 'Course Credits', 'Grade'], index=False, header=False)
+            row += df.shape[0]+2
+        writer.save()
+        
+        
+    
+    def get_table_from_html(self, data):
         soup = BeautifulSoup(data, 'html.parser')
         tables = soup.findAll('table', attrs={'width':'900'})
         semester_data = []
@@ -44,7 +68,7 @@ class DegreeCalc:
             semester_data.append(courses_data)
         sem_data_merged = pd.concat(semester_data)
         sem_data_merged = sem_data_merged.sort_values(by=['Course Category'])
-        print(sem_data_merged)
+        return sem_data_merged
     
     def get_grades_url(self, data):
         soup = BeautifulSoup(data, 'html.parser')
@@ -73,13 +97,13 @@ class DegreeCalc:
         
         # self.test_func()
         # grades = self.response
-        table = self.get_table_from_text(grades)
+        table = self.get_table_from_html(grades)
+        self.generate_gradesheet(table)
         
         
     def test_func(self):
         self.response = """
        
-
         """
     
 if __name__=='__main__':
